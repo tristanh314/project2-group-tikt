5e481082fc023101c7f7d70d400a80dc94b6d942
TySorensen <ty.sorensen@callruby.com>
2020-04-20 20:48:41 -0700

    initial base line chart

---
+++ ty_app.js
@@ -0,0 +1,70 @@
+
+
+d3.json("static/data/simpson_episodes.json").then(function(simpsonsData) 
+    { console.log(simpsonsData.result[0].season)
+
+    function updateChart() {
+      var selection = d3.select("#seasons").node().value;   
+
+      var selectedSeason =  simpsonsData.result.filter(function(episode) {
+        return episode.season == selection;
+      });
+      console.log(selectedSeason)
+      
+      selectedSeason = selectedSeason.sort((a, b) => (a.number_in_season > b.number_in_season) ? 1 : -1)
+
+      var views = selectedSeason.map( d => d.us_viewers_in_millions)
+      var episode = selectedSeason.map( d => d.number_in_season)
+
+      new Chart(document.getElementById("tyChart"), {
+        type: 'line',
+        data: {
+          labels: episode,
+          datasets: [{ 
+              data: views,
+              label: "US Viewers (millions)",
+              borderColor: "#3e95cd",
+              fill: false
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: false,
+          }
+        }
+      });
+    } 
+
+      d3.selectAll("#seasons").on("change", updateChart);
+      updateChart();
+    });
+
+    
+// // init function to load chart with page load data
+// // update chart function. Take a paramater based on selection and update chart based on selection. 
+// // Set an event listner for user Input. When that changes it runs chart again on new value. 
+
+// // chart 3 build information
+new Chart(document.getElementById("tyChart"), {
+    type: 'line',
+    data: {
+      labels: number_in_season,
+      datasets: [{ 
+          data: us-viewers_in_millions,
+          label: "test",
+          borderColor: "#3e95cd",
+          fill: false
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: false,
+      }
+    }
+  });
+
+
+
+
