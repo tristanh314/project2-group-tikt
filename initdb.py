ddf55ddea66199f13752632188cadfa9363e22b1
tristanh314 <tristanh314@gmail.com>
2020-04-22 23:25:57 -0700

    Redid init.py.

--- initdb.py
+++ initdb.py
@@ -2,66 +2,63 @@
 # coding: utf-8
 
 # Import dependencies
-from flask_pymongo import PyMongo
-from flask import Flask
 import pandas as pd
 import json
 import pymongo
 import os
-from simpson_dashboard.main import mongo
+import sys
 
-# Function to import the script data.
-def import_content():
-    lines = mongo.db.simpson_script
+# Set connection uri
+# uri = "mongodb://heroku_1bchz9m0:8ptku4qco1bq0ednuc33ag3gva@ds139619.mlab.com:39619/heroku_1bchz9m0"
+uri = os.environ.get('MONGODB_URI', '') or "mongodb://localhost:27017/simpson_data"
+
+# Function to import the data.
+def main(args):
+    
+    # Initialize a client.
+    client = pymongo.MongoClient(uri)
+
+    # Get the database
+    db = client.get_default_database()
+    
+    # Import the script data.
+    lines = db["simpson_script"]
     file_res = os.path.join("clean_and_load", "data", "simpson_script_clean.csv")
 
     data = pd.read_csv(file_res)
     data_json = json.loads(data.to_json(orient='records'))
     lines.drop()
     lines.insert_many(data_json)
-    return None
 
-# Import the script data.
-import_content()
-
-# Function to import the episode data
-def import_episodes():
-    episodes = mongo.db.simpson_episodes
+    # Import the episode data.
+    episodes = db["simpson_episodes"]
     file_res = os.path.join("clean_and_load", "data", "simpsons_episodes.csv")
-
     data = pd.read_csv(file_res)
     data_json = json.loads(data.to_json(orient='records'))
     episodes.drop()
     episodes.insert_many(data_json)
-    return None
 
-# Import the episode data.
-import_episodes()
-
-# Function to import location data.
-def import_location():
-    locations = mongo.db.simpson_location
+    # Import location data.
+    locations = db["simpson_location"]
     file_res = os.path.join("clean_and_load", "data", "simpsons_locations.csv")
-
     data = pd.read_csv(file_res)
     data_json = json.loads(data.to_json(orient='records'))
     locations.drop()
     locations.insert_many(data_json)
-    return None
+    
 
-# Import the location data.
-import_location()
-
-# Function to import the character data.
-def import_character():
-    characters = mongo.db.simpson_character
+    # Import the character data.
+    characters = db["simpson_character"]
     file_res = os.path.join("clean_and_load", "data", "simpsons_characters.csv")
-
     data = pd.read_csv(file_res)
     data_json = json.loads(data.to_json(orient='records'))
     characters.drop()
     characters.insert_many(data_json)
-    return None
+
+    # Close the connection
+    client.close()
     
-# Import the character data.
-import_character()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
