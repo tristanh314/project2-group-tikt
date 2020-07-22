0a91a9889c3c035bcae5085ae0dfd1f4715b14e4
tristanh314 <tristanh314@gmail.com>
2020-07-21 17:06:11 -0700

    Struggling to get environment variables available in app.

--- initdb.py
+++ initdb.py
@@ -7,58 +7,50 @@ import json
 import pymongo
 import os
 import sys
+import dns
+from decouple import config
 
 # Set connection uri
-uri = "mongodb://heroku_1bchz9m0:8ptku4qco1bq0ednuc33ag3gva@ds139619.mlab.com:39619/heroku_1bchz9m0"
-# uri = os.environ.get('MONGODB_URI', '') or "mongodb://localhost:27017/simpson_data"
+# URI = config('SIMPSONS_URI') or "mongodb://localhost:27017/simpson_data"
+print(URI)
+# Initialize a client.
+client = pymongo.MongoClient(URI)
 
-# Function to import the data.
-def main(args):
+# Get the database
+db = client.simpson_data
     
-    # Initialize a client.
-    client = pymongo.MongoClient(uri)
+# Import the script data.
+lines = db["simpson_script"]
+file_res = os.path.join("clean_and_load", "data", "simpson_script_clean.csv")
 
-    # Get the database
-    db = client.get_default_database()
+data = pd.read_csv(file_res)
+data_json = json.loads(data.to_json(orient='records'))
+lines.drop()
+lines.insert_many(data_json)
+
+# Import the episode data.
+episodes = db["simpson_episodes"]
+file_res = os.path.join("clean_and_load", "data", "simpsons_episodes.csv")
+data = pd.read_csv(file_res)
+data_json = json.loads(data.to_json(orient='records'))
+episodes.drop()
+episodes.insert_many(data_json)
+
+# Import location data.
+locations = db["simpson_location"]
+file_res = os.path.join("clean_and_load", "data", "simpsons_locations.csv")
+data = pd.read_csv(file_res)
+data_json = json.loads(data.to_json(orient='records'))
+locations.drop()
+locations.insert_many(data_json)
     
-    # Import the script data.
-    lines = db["simpson_script"]
-    file_res = os.path.join("clean_and_load", "data", "simpson_script_clean.csv")
+# Import the character data.
+characters = db["simpson_character"]
+file_res = os.path.join("clean_and_load", "data", "simpsons_characters.csv")
+data = pd.read_csv(file_res)
+data_json = json.loads(data.to_json(orient='records'))
+characters.drop()
+characters.insert_many(data_json)
 
-    data = pd.read_csv(file_res)
-    data_json = json.loads(data.to_json(orient='records'))
-    lines.drop()
-    lines.insert_many(data_json)
-
-    # Import the episode data.
-    episodes = db["simpson_episodes"]
-    file_res = os.path.join("clean_and_load", "data", "simpsons_episodes.csv")
-    data = pd.read_csv(file_res)
-    data_json = json.loads(data.to_json(orient='records'))
-    episodes.drop()
-    episodes.insert_many(data_json)
-
-    # Import location data.
-    locations = db["simpson_location"]
-    file_res = os.path.join("clean_and_load", "data", "simpsons_locations.csv")
-    data = pd.read_csv(file_res)
-    data_json = json.loads(data.to_json(orient='records'))
-    locations.drop()
-    locations.insert_many(data_json)
-    
-
-    # Import the character data.
-    characters = db["simpson_character"]
-    file_res = os.path.join("clean_and_load", "data", "simpsons_characters.csv")
-    data = pd.read_csv(file_res)
-    data_json = json.loads(data.to_json(orient='records'))
-    characters.drop()
-    characters.insert_many(data_json)
-
-    # Close the connection
-    client.close()
-    
-
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
+# Close the connection
+client.close()
